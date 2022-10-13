46a55b3809971cd87c04b4a4ea6004ee9d9b5e95
Gummimundur <gudm.mar@hotmail.com>
2022-10-13 23:59:45 +0000

    INFRA: Initialize Unit Test Project

---
+++ Grammar.Checker.Portal.Web.Tests.Unit/Services/DeleteMe.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Grammar.Checker.Portal.Web.Tests.Unit.Services
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
