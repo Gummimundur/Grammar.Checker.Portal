3c15d3250f3739e9af858abdbf4125406fed5c23
Gummimundur <gudm.mar@hotmail.com>
2022-10-16 18:18:02 +0000

    BASES: Container Base

---
+++ Grammar.Checker.Portal.Web/Views/Bases/ContainerBase.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Grammar.Checker.Portal.Web.Views.Bases
+{
+    public partial class ContainerBase : ComponentBase
+    {
+        [Parameter]
+        public string Style { get; set; }
+
+        [Parameter]
+        public string CssClass { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
