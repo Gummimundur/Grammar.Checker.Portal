5329b3274e0d992e42ec9f54b18aa5e3e0700d70
Gummimundur <gudm.mar@hotmail.com>
2022-10-16 18:11:23 +0000

    BASES: Header Base

---
+++ Grammar.Checker.Portal.Web/Views/Bases/HeaderBase.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Grammar.Checker.Portal.Web.Views.Bases
+{
+    public partial class HeaderBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string ClassName { get; set; }
+    }
+}
