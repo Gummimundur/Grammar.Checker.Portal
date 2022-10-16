163da0d5b18c2ba065bcac7db1144af33cfb0ddb
Gummimundur <gudm.mar@hotmail.com>
2022-10-16 19:10:48 +0000

    BASES: Large Title Base

---
+++ Grammar.Checker.Portal.Web/Views/Bases/LargeTitleBase.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Grammar.Checker.Portal.Web.Views.Bases
+{
+    public partial class LargeTitleBase : ComponentBase
+    {
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public string ClassName { get; set; }
+    }
+}
