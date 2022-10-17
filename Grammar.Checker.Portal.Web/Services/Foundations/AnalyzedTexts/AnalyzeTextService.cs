45d07785adc30d93d3dc2d4414d60d07b8ecb0c6
Gummimundur <gudm.mar@hotmail.com>
2022-10-17 20:41:33 +0000

    CODERUB: Move Mappings To Partial Class

--- Grammar.Checker.Portal.Web/Services/Foundations/AnalyzedTexts/AnalyzeTextService.cs
+++ Grammar.Checker.Portal.Web/Services/Foundations/AnalyzedTexts/AnalyzeTextService.cs
@@ -3,15 +3,11 @@ using Grammar.Checker.Portal.Web.Brokers.Loggings;
 using Grammar.Checker.Portal.Web.Models.Services.Foundations.AnalyzedText;
 using Grammar.Checker.Portal.Web.Models.Services.Foundations.ExternalAnalyzedText;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Grammar.Checker.Portal.Web.Services.Foundations.AnalyzedTexts
 {
-    public class AnalyzeTextService : IAnalyzeTextService
+    public partial class AnalyzeTextService : IAnalyzeTextService
     {
         private readonly IExternalTextAnalyzerBroker externalTextAnalyzerBroker;
         private readonly ILoggingBroker loggingBroker;
@@ -42,68 +38,9 @@ namespace Grammar.Checker.Portal.Web.Services.Foundations.AnalyzedTexts
             return externalAnalyxedText;
         }
 
-        private static AnalyzedText AsAnalyzedText(ExternalAnalyzedText externalAnalyzedText)
+        private static string SerializeText(string textToSerialize)
         {
-            return new AnalyzedText
-            {
-                Suggestions = AsSuggestions(externalAnalyzedText),
-                Text = externalAnalyzedText.Text
-            };
-        }
-
-        private static List<Suggestion> AsSuggestions(ExternalAnalyzedText externalAnalyzedText)
-        {
-            IEnumerable<Result> result = 
-                externalAnalyzedText.Result.SelectMany(result => result);
-
-            return externalAnalyzedText.Result.SelectMany(result => result).Select(result =>
-            {
-                return new Suggestion
-                {
-                    Explanations = AsExplanation(result),
-                    CorrectedText = result.Corrected,
-                    OriginalText = result.Original,
-                    Tokens = AsTokens(result)
-                };
-            }).ToList();
-        }
-
-        private static List<Token> AsTokens(Result result)
-        {
-            return result.Symbols.Select(symbol =>
-            {
-                return new Token
-                {
-                    WordIndex = symbol.WordIndex,
-                    TokenType = AsTokenType(symbol),
-                    OriginalText = symbol.OriginalText,
-                    CorrectedText = symbol.CorrectedText
-                };
-            }).ToList();
-        }
-
-        private static TokenType AsTokenType(Symbol symbol) =>
-            (TokenType) symbol.SymbolType;
-
-        private static List<Explanation> AsExplanation(Result result)
-        {
-            return result.Annotations.Select(annotation =>
-            {
-                return new Explanation
-                {
-                    Code = annotation.Code,
-                    Detail = annotation.Detail,
-                    EndCharacterIndex = annotation.EndCharacter,
-                    StartCharacterIndex = annotation.StartCharacter,
-                    Suggestion = annotation.Suggest,
-                    Text = annotation.Text
-                };
-            }).ToList();
-        }
-
-        private static string SerializeText(string text)
-        {
-            object objectToSerialize = new { text };
+            object objectToSerialize = new { text = textToSerialize };
 
             return JsonConvert.SerializeObject(objectToSerialize);
         }
