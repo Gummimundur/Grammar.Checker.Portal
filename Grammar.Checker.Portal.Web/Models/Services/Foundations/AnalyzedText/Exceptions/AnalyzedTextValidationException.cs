f5be519898419ef13ea07123afeb6f46c7484ad7
Gummimundur <gudm.mar@hotmail.com>
2022-10-19 16:35:22 +0000

    ShouldThrowValidationExceptionIfTextIsInvalidAndLogItAsync -> FAIL

---
+++ Grammar.Checker.Portal.Web/Models/Services/Foundations/AnalyzedText/Exceptions/AnalyzedTextValidationException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace Grammar.Checker.Portal.Web.Models.Services.Foundations.AnalyzedText.Exceptions
+{
+    public class AnalyzedTextValidationException : Xeption
+    {
+        public AnalyzedTextValidationException(Xeption innerException)
+            : base(message: "Analyzed text validation error occurred",
+                  innerException)
+        { }
+    }
+}
---
+++ Grammar.Checker.Portal.Web/Models/Services/Foundations/AnalyzedText/Exceptions/InvalidAnalyzedTextException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace Grammar.Checker.Portal.Web.Models.Services.Foundations.AnalyzedText.Exceptions
+{
+    public class InvalidAnalyzedTextException : Xeption
+    {
+        public InvalidAnalyzedTextException()
+            : base(message: "Invalid analyzed text error occurred")
+        { }
+    }
+}
